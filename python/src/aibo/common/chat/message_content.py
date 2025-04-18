ae5244107e9a08e795a74426acbb0ce6d6c48584
dmed256 <dmed256@gmail.com>
2025-04-17 21:04:39 -0700

    [python] Fix black

--- python/src/aibo/common/chat/message_content.py
+++ python/src/aibo/common/chat/message_content.py
@@ -46,10 +46,12 @@ class BaseMessageContent(BaseModel, abc.ABC):
     kind: str
 
     @abc.abstractmethod
-    async def to_openai(self) -> openai_chat.ChatCompletionContentPartParam | None: ...
+    async def to_openai(self) -> openai_chat.ChatCompletionContentPartParam | None:
+        ...
 
     @abc.abstractmethod
-    def __str__(self) -> str: ...
+    def __str__(self) -> str:
+        ...
 
 
 class TextMessageContent(BaseMessageContent):
